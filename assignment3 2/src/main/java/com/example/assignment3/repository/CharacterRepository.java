78b4f7aa38916c7cbe2079d97d18fec0803633d6
ieuceda26 <ieuceda26@gmail.com>
2026-07-05 21:48:14 -0400

    files added and tested endpoints

---
+++ assignment3 2/src/main/java/com/example/assignment3/repository/CharacterRepository.java
@@ -0,0 +1,17 @@
+package com.example.assignment3.repository;
+
+import com.example.assignment3.entity.Character;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CharacterRepository extends JpaRepository<Character, Long> {
+
+    // Get all characters of a specific universe
+    List<Character> findByUniverse(String universe);
+
+    // Get characters whose name contains a specific string
+    List<Character> findByNameContainingIgnoreCase(String name);
+}
